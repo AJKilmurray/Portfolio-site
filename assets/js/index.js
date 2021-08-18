3c59a2058a6dd3fcfa3bd66aedc8854b20515efe
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-08-18 22:30:23 +0100

    Portfolio Completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,32 +2,36 @@ import config from "./config.js";
 
 const projectsContainer = document.getElementById("project-list");
 const filters = document.querySelectorAll(".filter-btn");
+const navToggle = document.querySelector(".navbar-toggler");
+const navIcon = document.querySelector(".navbar-toggler-icon");
 
-formatProjects(config.projects);
+// Initializes projects upon page load/refresh
+formatProjects();
 
+navToggle.addEventListener("click", toggleNavColor);
 filters.forEach((btn) => btn.addEventListener("click", filterProjects));
 
 // Filter projects based on the tech category clicked
 function filterProjects(event) {
-  const keyword = event.currentTarget.dataset.tech;
+	const keyword = event.currentTarget.dataset.tech;
 
-  const results = [];
-  config.projects.forEach((project) => {
-    for (let tech of project.tech) {
-      if (tech === keyword) results.push(project);
-    }
-  });
+	const results = [];
+	config.projects.forEach((project) => {
+		for (let tech of project.tech) {
+			if (tech === keyword) results.push(project);
+		}
+	});
 
-  formatProjects(results);
+	formatProjects(results);
 }
 
 // Parse Projects into HTML formatted strings
-function formatProjects(projects) {
-  const format = projects
-    .map((project) => {
-      const icons = project.tech.map((tech) => `<img src="assets/images/tech-icons/${tech}-icon.png" />`).join("");
+function formatProjects(projects = config.projects) {
+	const format = projects
+		.map((project) => {
+			const icons = project.tech.map((tech) => `<img src="assets/images/tech-icons/${tech}-icon.png" />`).join("");
 
-      return `<div class="col-md-4 project-card">
+			return `<div class="col-md-4 project-card">
       <article class="project-content">
         <h1 class="font-primary">${project.title}</h1>
         <h2 class="font-primary project-date">${project.date}</h2>
@@ -49,25 +53,36 @@ function formatProjects(projects) {
         <a href="${project.repo}" target="_blank" class="btn project-link"><span class="text-highlight">View</span> Code</a>
       </article>
     </div>`;
-    })
-    .join("");
+		})
+		.join("");
 
-  displayProjects(format);
+	displayProjects(format);
 }
 
 function displayProjects(projects) {
-  while (projectsContainer.firstChild) {
-    projectsContainer.removeChild(projectsContainer.lastChild);
-  }
+	while (projectsContainer.firstChild) {
+		projectsContainer.removeChild(projectsContainer.lastChild);
+	}
 
-  if (!projects) {
-    noProjectsFound();
-    return;
-  }
+	if (!projects) {
+		noProjectsFound();
+		return;
+	}
 
-  projectsContainer.insertAdjacentHTML("afterbegin", projects);
+	projectsContainer.insertAdjacentHTML("afterbegin", projects);
 }
 
 function noProjectsFound() {
-  projectsContainer.insertAdjacentHTML("afterbegin", `<h3 class="projects-error font-primary">${config.error}</h3>`);
+	projectsContainer.insertAdjacentHTML("afterbegin", `<h3 class="projects-error font-primary">${config.error}</h3>`);
+}
+
+// Navbar Toggler
+function toggleNavColor(event) {
+	const status = event.currentTarget.ariaExpanded === "true" ? true : false;
+
+	if (status) {
+		navIcon.classList.add("active-toggle");
+	} else if (!status) {
+		navIcon.classList.remove("active-toggle");
+	}
 }
